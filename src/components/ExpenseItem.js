33c2e480999c8ecf69143ff0df83630a954a861b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-14 17:22:05 +0300

    Passed Data in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,12 @@
-const ExpenseItem=()=>{
-    return(
-        <h1>Hello Expenses</h1>
-    )
-}
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>Date 28th 2021</div>
+      <div>
+        <h2>Title</h2>
+      </div>
+      <div>$294.67</div>
+    </div>
+  );
+};
 export default ExpenseItem;
