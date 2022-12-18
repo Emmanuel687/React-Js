8a5d76e12e6723560898cceedc59978800458c01
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-18 15:30:41 +0300

    Added Expense-date_day className

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,9 +7,9 @@ const ExpenseDate = (props) => {
     const year = props.date.getFullYear();
   return (
     <div className="expense-date">
-      <div className="expense-date_month">{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
