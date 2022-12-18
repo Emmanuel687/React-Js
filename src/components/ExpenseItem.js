78b64b8d13df31d199cf141e93c2760c1a756c2b
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-18 15:14:08 +0300

    Passed props Expense Date Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+ 
   return (
     <div className="expense-item">
+       <ExpenseDate date = {props.date}>
+
+       </ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
