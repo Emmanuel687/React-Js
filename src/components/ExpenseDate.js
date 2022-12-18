94685f3a22208cb7016049a9f5d79389764b3fa4
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-18 15:00:15 +0300

    Added Javascript logic in Expense ate

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const ExpenseDate = () => {
-    
+    const month = props.date.toLocaleString(`en-Us`,{month: `long`});
+    const day = props.date.toLocaleString(`en-US`,{day: `2-digit`});
+    const year = props.date.getFullYear();
   return (
     <div>
       <div>{month}</div>
