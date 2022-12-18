6d72680cf087ca63bca2067f964ca10842a22a22
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2022-12-18 15:27:51 +0300

    Added Expense-date className

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,13 @@
 import React from "react";
+import './Expense.css';
 
 const ExpenseDate = (props) => {
     const month = props.date.toLocaleString(`en-Us`,{month: `long`});
     const day = props.date.toLocaleString(`en-US`,{day: `2-digit`});
     const year = props.date.getFullYear();
   return (
-    <div>
-      <div>{month}</div>
+    <div className="expense-date">
+      <div className="">{month}</div>
       <div>{day}</div>
       <div>{year}</div>
     </div>
