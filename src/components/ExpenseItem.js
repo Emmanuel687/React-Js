e059d679e7d4ff784a5ccfe808194215b97f2fe7
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-01-09 21:17:29 +0300

    Added Event listern

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,17 +4,18 @@ import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
- 
+ const handlerClick = ()=>{
+  console.log(`Hello Emmanuel`)
+ }
   return (
     <Card className="expense-item">
-       <ExpenseDate date = {props.date}>
+       <ExpenseDate date = {props.date} />
 
-       </ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
-      <button onlick={console.log(`clicked`)}>Change Title</button>
+      <button onClick={handlerClick}>Change Title</button>
     </Card>
   );
 };
